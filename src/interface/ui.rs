8ddd5c74df8e20644454ee40617dab16826935ac
Oskar Nyberg <me@oskarnyberg.com>
2022-10-15 14:59:04 +0200

    Make quit into an event

--- src/interface/ui.rs
+++ src/interface/ui.rs
@@ -5,6 +5,12 @@ use tokio_stream::StreamExt;
 use tokio::{select, sync::mpsc};
 use tui::Terminal;
 
+#[derive(Debug)]
+enum UpdateEvent {
+    Redraw,
+    Quit,
+}
+
 pub async fn run(terminal: &mut Terminal<Backend>, app: &mut App) {
     let mut event_reader = EventStream::new();
     let (tx, mut rx) = mpsc::channel(100);
@@ -13,25 +19,34 @@ pub async fn run(terminal: &mut Terminal<Backend>, app: &mut App) {
 
     loop {
         select! {
-            _ = rx.recv() => {
-                run_draw_cycle(terminal, app);
+            Some(event) = rx.recv() => {
+                match event {
+                    UpdateEvent::Redraw => run_draw_cycle(terminal, app),
+                    UpdateEvent::Quit => break,
+                };
             },
-
-            Some(Ok(event)) = event_reader.next() => {
-                if let Event::Key(KeyEvent { code: KeyCode::Char('q'), .. }) = event {
-                    break;
-                } else {
-                    app.handle_event_sync(event.clone());
-                    let future = app.handle_event(event.clone());
-
-                    let tx = tx.clone();
-                    tokio::spawn(async move {
-                        future.await;
-                        tx.send(()).await.expect("Failed to send draw event");
-                    });
-                }
-            },
+            Some(Ok(event)) = event_reader.next() => handle_event(tx.clone(), terminal, app, event),
         };
+    }
+}
+
+fn handle_event(
+    tx: mpsc::Sender<UpdateEvent>,
+    terminal: &mut Terminal<Backend>,
+    app: &mut App,
+    event: Event,
+) {
+    if let Event::Key(KeyEvent { code: KeyCode::Char('q'), .. }) = event {
+        tx.try_send(UpdateEvent::Quit).expect("Failed to send quit event");
+    } else {
+        app.handle_event_sync(event.clone());
+        let future = app.handle_event(event.clone());
+
+        let tx = tx.clone();
+        tokio::spawn(async move {
+            future.await;
+            tx.send(UpdateEvent::Redraw).await.expect("Failed to send draw event");
+        });
     }
 }
 
